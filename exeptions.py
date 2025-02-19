f3e0640a30144dc6dda2e2769421fa76ab6bbca1
Nikita Akulin <bmww52@yandex.ru>
2025-02-19 19:09:41 +0300

    6

---
+++ exeptions.py
@@ -0,0 +1,8 @@
+class ConectionApiError(Exception):
+    """Ошибка подключения к API."""
+
+class ResponseError(Exception):
+    """Ошибка статус кода от API."""
+
+class TelegramSendMessageError(Exception):
+    """Ошибка отправки сообщения в телеграм"""
